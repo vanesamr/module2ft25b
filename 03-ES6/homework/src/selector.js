c64dbf0ad89f9a20d0788c62353e2df35e2ac05a
Vanesa <76010613+vanesamr@users.noreply.github.com>
2022-05-04 12:19:59 -0300

    Homework 03 done

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl)) resultSet.push(startEl);
   
+  if(startEl.children.length){
+    for (let el of startEl.children){
+      const result = traverseDomAndCollectElements(matchFunc, el)
+      resultSet = [...resultSet,...result]
+    }
+  }
+return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +26,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  if(selector[0] === '.') return 'class';
+  if(selector.split('.').length > 1) return 'tag.class';
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,14 +40,19 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
-  } else if (selectorType === "tag") {
-    
+  if (selectorType === "id") { matchFunction = (element) => 
+    `#${element.id}` === selector; 
+   } else if (selectorType === "class") { matchFunction = (element) => 
+    element.classList.contains(selector.replace('.',''));
+   } else if (selectorType === "tag.class"){
+      matchFunction = function (element){
+        const [tag, tagClass] = selector.split('.')
+      return(
+        matchFunctionMaker(tag)(element)&&
+        matchFunctionMaker(`.${tagClass}`)(element)
+      )};
+   } else if (selectorType === "tag") { matchFunction = (element) => 
+    element.tagName.toLowerCase() === selector.toLowerCase();
   }
   return matchFunction;
 };
